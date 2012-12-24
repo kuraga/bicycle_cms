c70333418e8e9822182e67db114b20deb12c4f14
Kurakin Alexander <kuraga333@mail.ru>
2012-12-24 14:45:20 +0400

    Налажена загрузка локалей

--- lib/bicycle_cms/engine.rb
+++ lib/bicycle_cms/engine.rb
@@ -2,8 +2,10 @@ module BicycleCms
   class Engine < ::Rails::Engine
     isolate_namespace BicycleCms
 
-    initializer 'bicycle_cms.autoload', :before => :set_autoload_paths do |app|
-      app.config.autoload_paths += Dir["#{Rails.root}/vendor/engines/bicycle_cms/app/models/**/*"]
+    initializer 'bicycle_cms.autoload', before: :set_autoload_paths do |app|
+      config.i18n.load_path += Dir["#{BicycleCms::Engine.root}/config/locales/**/*.{rb,yml}"]
+      config.autoload_paths += Dir[File.expand_path("../app/models", __FILE__) + "/**/*.rb"]
     end
+
   end
 end
