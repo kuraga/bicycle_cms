2c24fc0537a2895ce819974b351e2965433d8961
Kurakin Alexander <kuraga333@mail.ru>
2013-01-22 19:55:41 +0400

    Маленький фикс

--- app/models/bicycle_cms/article.rb
+++ app/models/bicycle_cms/article.rb
@@ -31,8 +31,9 @@ module BicycleCms
     friendly_id :title, use: :slugged
     should_not_generate_new_friendly_id_unless_new_record_and_blank_slug!
 
-    # FIXME col :product_id, as: :primary_key
-    inherits_from :product, methods: true # FIXME
+    # FIXME
+    col :product_id, as: :integer # FIXME :primary_key
+    inherits_from :product, methods: true
 
   end
 end
